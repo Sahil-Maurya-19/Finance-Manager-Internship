f4d86288f95f16346908748ee4cad95f3ff0536c
Sahil-Maurya-19 <sahilmaurya0819@gmail.com>
2025-03-10 14:05:29 +0530

    Initial commit

--- backend/routes/transactionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addTransaction } = require('../controllers/transactionController');
-
-router.post('/', addTransaction);
-
-module.exports = router;
